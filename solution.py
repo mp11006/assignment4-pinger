b9cae52c032010e5b1f27177723a0e10d0962149
mp11006 <98921624+mp11006@users.noreply.github.com>
2022-04-15 11:40:27 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -16,13 +16,13 @@ def checksum(string):
     count = 0
 
     while count < countTo:
-        thisVal = ord(string[count + 1]) * 256 + ord(string[count])
+        thisVal = (string[count + 1]) * 256 + (string[count])
         csum = csum + thisVal
         csum = csum & 0xffffffff
         count = countto + 2
 
     if countTo < len(string):
-        csum = csum + ord(string[len(string) - 1])
+        csum = csum + (string[len(string) - 1])
         csum = csum & 0xffffffff
 
     csum = (csum >> 16) + (csum & 0xffff)
